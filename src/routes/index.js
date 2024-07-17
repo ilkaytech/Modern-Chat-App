3422f3cff6156040066f19a92effb7000a23c6c9
İlkay Öztürk <118361845+ilkaytech@users.noreply.github.com>
2024-07-17 21:26:37 +0300

    form management has been created

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { Navigate, useRoutes } from "react-router-dom";
 
 // layouts
 import DashboardLayout from "../layouts/dashboard";
+import MainLayout from "../layouts/main";
 
 // config
 import { DEFAULT_PATH } from "../config";
@@ -18,6 +19,11 @@ const Loadable = (Component) => (props) => {
 
 export default function Router() {
   return useRoutes([
+    {
+      path: "/auth",
+      element: <MainLayout />,
+      children: [{ element: <LoginPage />, path: "login" }],
+    },
     {
       path: "/",
       element: <DashboardLayout />,
@@ -37,6 +43,7 @@ export default function Router() {
 const GeneralApp = Loadable(
   lazy(() => import("../pages/dashboard/GeneralApp"))
 );
+const LoginPage = Loadable(lazy(() => import("../pages/auth/Login")));
 const Settings = Loadable(lazy(() => import("../pages/dashboard/Settings")));
 
 const Page404 = Loadable(lazy(() => import("../pages/Page404")));
