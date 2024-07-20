a5378707b24867861a927ec68117de39a281a1dd
İlkay Öztürk <118361845+ilkaytech@users.noreply.github.com>
2024-07-20 20:16:43 +0300

    user profil page created

---
+++ src/sections/settings/ProfileForm.js
@@ -0,0 +1,91 @@
+import React, { useCallback } from "react";
+import * as Yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Stack, Alert, Button } from "@mui/material";
+import FormProvider from "../../components/hook-form/FormProvider";
+import { RHFTextField } from "../../components/hook-form";
+
+const ProfileForm = () => {
+  const ProfileSchema = Yup.object().shape({
+    firstName: Yup.string().required("Name is required"),
+    about: Yup.string().required("About is required"),
+    avatar: Yup.string().required("Avatar is required").nullable(true),
+  });
+
+  const defaultValues = {
+    name: "",
+    about: "",
+  };
+
+  const methods = useForm({
+    resolver: yupResolver(ProfileSchema),
+    defaultValues,
+  });
+
+  const {
+    reset,
+    watch,
+    control,
+    setValue,
+    setError,
+    handleSubmit,
+    formState: { errors, isSubmitting, isSubmitSuccessful },
+  } = methods;
+
+  const values = watch();
+
+  const handleDrop = useCallback(
+    (acceptedFiles) => {
+      const file = acceptedFiles[0];
+
+      const newFile = Object.assign(file, {
+        preview: URL.createObjectURL(file),
+      });
+
+      if (file) {
+        setValue("avatar", newFile, { shouldValidate: true });
+      }
+    },
+    [setValue]
+  );
+
+  const onSubmit = async (data) => {
+    try {
+      // submit data to backend
+      console.log("Data", data);
+    } catch (error) {
+      console.error(error);
+      reset();
+      setError("afterSubmit", {
+        ...error,
+        message: error.message,
+      });
+    }
+  };
+
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={4}>
+        <Stack spacing={3}>
+          {!!errors.afterSubmit && (
+            <Alert severity="error">{errors.afterSubmit.message}</Alert>
+          )}
+          <RHFTextField
+            helperText={"This name is visible to your contacts"}
+            name="firstName"
+            label="First Name"
+          />
+          <RHFTextField multiline rows={4} name="about" label="About" />
+        </Stack>
+        <Stack direction={"row"} justifyContent="end">
+          <Button color="primary" size="large" type="submit" variant="outlined">
+            Save
+          </Button>
+        </Stack>
+      </Stack>
+    </FormProvider>
+  );
+};
+
+export default ProfileForm;
